0ecb86d096369d125474009a10e1b29bc0000cf3
Georgiy Danielyan <georgiyd91@gmail.com>
2015-12-08 16:29:54 -0800

    Initial Commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -80,6 +80,8 @@ $app->singleton(
 
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+// $app->register('Collective\HTML\HtmlServiceProvider');
+
 
 /*
 |--------------------------------------------------------------------------
--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1,48 +1,64 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="UTF-8" content="width=device-width, initial-scale=1">
 	<title>Degrees Web Service</title>
-	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
-
-		body {
-			margin:0;
-			font-family:'Lato', sans-serif;
-			color: #999;
-			font-size:14px;
-		}
-
-		.welcome {
-			position: absolute;
-			left: 10px;
-			top: 10px;
-		}
-
-		a, a:visited {
-			text-decoration:none;
-		}
-
-		h1 {
-			margin: 16px 0 0 0;
-		}
-
-		dl {width:600px;}
-		dt {font-weight:bold;font-size:16px;margin-top:20px;}
-		dd {margin:0;font-weight:normal;}
-	</style>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="{{ url('css/styles.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ url('css/prism.css')}}">
 </head>
 <body>
-	<div class="welcome">
-		<h1>Degrees Web Service</h1>
-
-		<h2>Examples</h2>
-		<dl>
-			<dt>Get all degrees for an individual</dt>
-			<dd><a href="{{ url('degrees?person=steven.fitzgerald@csun.edu') }}">
-				{{ url('degrees?person=steven.fitzgerald@csun.edu') }}
-			</a></dd>
-		</dl>
+	<div class="container">
+		<div id="page-header" class="row">
+			<h1>Degrees<br><span>Web Service&nbsp;</span></h1>
+		</div>
+		<div id="body-content">
+			<div id="introduction" class="row">
+				<h3>Introduction</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p id="json-data">
+					<pre class="language-json pull-right">
+[
+	{
+		"degree":"Ph.D.",
+		"year":1995,
+		"institute":"University of Massaschusetts","
+	},
+	{
+		"degree":"M.S.",
+		"year":1990,
+		"institute":"University of Massaschusetts",
+	{
+		"degree":"B.S.",
+		"year":1986,
+		"institute":"Lowell University",
+	}
+]
+					</pre>
+				</p>
+			</div>
+			<div id="how-to" class="row">
+				<h3>How to</h3>
+				<ol>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ol>
+			</div>
+			<div id="example" class="row">
+				<h3>Examples</h3>
+				<dl>
+					<dt>Get all degrees for an individual</dt>
+					<dd><a href="{{ url('degrees?person=steven.fitzgerald@csun.edu') }}">
+						{{ url('degrees?person=steven.fitzgerald@csun.edu') }}</a>
+					</dd>
+				</dl>
+			</div>
+		</div>
 	</div>
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="{{ url('js/prism.js') }}"type="text/javascript"></script>
 </html>
