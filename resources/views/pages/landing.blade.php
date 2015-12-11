6d6ee4427d63a01b7654e4c8637cebaf2406d2e3
Georgiy Danielyan <georgiyd91@gmail.com>
2015-12-10 22:41:25 -0800

    Nearly done with design.. content needs review

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -18,53 +18,49 @@
 				<h3>Introduction</h3>
 				<p>The degrees API provides a web service as an interface for requesting information about a professor's degree and institutional background.
 				This information is derived through CSUN's... The data is generated through a RESTful API by simply appending a name value pair at the end of a URI. 
-				The web service uses HTTP requests to specific URL's and the service generally returns data as a JSON object and contains information including degree, year, and institution.</p>
+				The web service uses HTTP requests to specific URL's and the service returns data in a form of JSON object that contains information including degree, year, and institution.</p>
 				<div id="json-data">An example of JSON object returned: 
-<pre class="language-json">
-[
-	{
-	    "degree": "Ph.D.",
-	    "discipline": null,
-	    "year": "2008",
-	    "institute": "University of California Los Angeles",
-	    "location": null,
-	    "latitude": null,
-	    "longitude": null
-	},
-	{
-	    "degree": "M.S.",
-	    "discipline": null,
-	    "year": "2003",
-	    "institute": "University of California Los Angeles",
-	    "location": null,
-	    "latitude": null,
-	    "longitude": null
-	},
-    {
-	    "degree": "B.S.",
-	    "discipline": null,
-	    "year": "2000",
-	    "institute": "University of California Los Angeles",
-	    "location": null,
-	    "latitude": null,
-	    "longitude": null
-    }
-]</pre>
+					<pre>
+						<code class="language-json">
+						[
+									{
+										"degree": "Ph.D.",
+										"year": "2008",
+										"institute": "University of California Los Angeles",
+									},
+
+									{
+										"degree": "M.S.",
+										"year": "2003",
+										"institute": "University of California Los Angeles",
+									},
+
+									{
+										"degree": "B.S.",
+										"year": "2000",
+										"institute": "University of California Los Angeles",
+									}
+						]
+						</code>
+				</pre>
 				</div>
 			</div>
 			<div id="how-to" class="row">
 				<h3>How to use</h3>
 				<p><span class="list-type">1</span>Generate a unique URI.<p>
-				<p><span class="list-type">2</span>Use the constructed URI to perform to request data.</p>
+				<p><span class="list-type">2</span>Use the constructed URI to send a query request.</p>
 				<p><span class="list-type">3</span>Browse through the returned JSON object and use as needed.</p>
 				<p class="see-below"><span class="glyphicon glyphicon-menu-down"></span> See below for a quick walk through.</p>
 				<dl>
 					<dt>Take this URI for example:</dt>
 					<dd>http://degrees.ptg.csun.edu/degrees?</dd>
+
 					<dt>Append a professors email address as the value to the key 'person': </dt>
 					<dd>person=steven.fitzgerald@csun.edu</dd>
-					<dt>Use the generated URI as a query:</dt>
+
+					<dt>Use the generated URL as a query:</dt>
 					<dd>http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu</dd>
+
 					<dt>Now, take a look at the possible results of the URL:</dt>
 					<dd>Using your language of choose you can now iterate over the JSON object and use as needed</dd>
 				</dl>
@@ -92,84 +88,103 @@
 					</ul>
 					<div class="tab-content">
 						<div role="tabpanel" class="tab-pane fade in active" id="php">
-<pre>
-<code class="language-php">
-$url = 'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu';
+							<pre>
+								<code class="language-php">
+								//generate a url
+								$url = 'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu';
 
-$data = file_get_contents($url);
+								//perform the query
+								$data = file_get_contents($url);
 
-$data = json_decode($data, true);
+								//decode the json
+								$data = json_decode($data, true);
 
-$degree_list = [];
+								//instantiate an empty array
+								$degree_list = [];
 
-foreach($data['degrees'] as $degree){
-	$degree_list = $degree['degree']. '' . $degree['institute'];
-}
-</code>
-</pre>
+								//iterate over the list of data 
+								foreach($data['degrees'] as $degree){
+										 $degree_list = $degree['degree']. '' . $degree['institute'];
+								}
+								</code>
+							</pre>
 						</div>
 						<div role="tabpanel" class="tab-pane fade" id="javascript">
-<pre>
-<code class="language-javascript">
-var url = 'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu';
-$(document).ready(function() {
+							<pre>
+								<code class="language-javascript">
+								//generate a url
+								var url = 'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu';
 
-	$.get(url, function(data) {
-		var degreeList = data.degrees;
+								//construct a function to get url and iterate over 
+								$(document).ready(function() {
 
-		$(degreeList).each(function(index, degree) {
+										//use the URL as a request
+										$.get(url, function(data) {
 
-			$('#degree-results').append(degree.degree + ' ' + degree.institute);
+												var degreeList = data.degrees;
+												//iterate over the degree list
+												$(degreeList).each(function(index, degree) {
+													
+														//append each degree and institute
+														$('#degree-results').append(degree.degree + ' ' + degree.institute);
 
-		});
-		
-	});
+												});
+										
+										});
 
-});
-</code>
-</pre>
+								});
+								</code>
+							</pre>
 						</div>
 						<div role="tabpanel" class="tab-pane fade" id="ruby">
-<pre>
-<code class="language-ruby">
-require 'net/http'
-require 'json'
+							<pre>
+								<code class="language-ruby">
+								require 'net/http'
+								require 'json'
 
-source = 'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu'
+								#generate a url
+								source = 'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu'
 
-response = Net::HTTP.get_response(URI.parse(source))
+								#request the data
+								response = Net::HTTP.get_response(URI.parse(source))
 
-data = response.body
+								#store the response in data variable
+								data = response.body
 
-#put the parsed data
-puts JSON.parse(data)
-</code>
-</pre>
+								#put the parsed data
+								puts JSON.parse(data)
+								</code>
+							</pre>
 						</div>
 						<div role="tabpanel" class="tab-pane fade" id="python">
-<pre>
-<code class="language-python">
-import urllib2
-import json
+							<pre>
+								<code class="language-python">
+								#python
+								import urllib2
+								import json
 
-url = u'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu'
+								#generate a url
+								url = u'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu'
 
-try:
-   u = urllib2.urlopen(url)
-   data = u.read()
-except Exception as e:
-	data = {}
+								#open the url 
+								try:
+									 u = urllib2.urlopen(url)
+									 data = u.read()
+								except Exception as e:
+									data = {}
 
-data = json.loads(data)
+								#load data with json object
+								data = json.loads(data)
 
-degrees_list = []
+								degrees_list = []
 
-for course in data['degree']:
-	degrees_list.append(degrees['degree'] + ' ' + degrees['institute'])
+								#iterate over the json object and add to the degrees list
+								for course in data['degree']:
+										 degrees_list.append(degrees['degree'] + ' ' + degrees['institute'])
 
-print degrees_list
-</code>
-</pre>
+								print degrees_list
+								</code>
+							</pre>
 						</div>
 					</div>
 				</div>
