b176a8109628c106501f71f1a39f50446d719ae1
klinster <contact@klinster.com>
2016-11-22 11:52:47 -0800

    Update the code blocks to prettyprint and update the layouts

--- resources/views/pages/landing-metaphor.blade.php
+++ resources/views/pages/landing-metaphor.blade.php
@@ -5,8 +5,8 @@
 	This information is derived through CSUN's catalog. The data is generated through a RESTful API by simply appending a name value pair at the end of a URI. 
 	The web service uses HTTP requests to specific URL's and the service returns data in a form of JSON object that contains information including degree, year, and institution.
 	<strong>An example of JSON object returned:</strong>
-	<pre>
-		<code>
+<pre>
+	<code class="prettyprint lang-json">
 {
 	appt_year: "1994",
 	degrees: [
@@ -27,8 +27,8 @@
 	}
 	]
 }
-						</code>
-					</pre>
+	</code>
+</pre>
 					<h2 id="how-to-use" class="type--header type--thin">How to use</h2>
 					<ol>
 						<li><strong>GENERATE A UNIQUE URI:</strong> Find the usage that fits your need. Browse through subcollections, instances and query types to help you craft your URI.</li>
@@ -38,7 +38,7 @@
 					<strong>Take this URI for example:</strong>
 					<div class="panel">
 						<div class="panel__content">
-						http://degrees.ptg.csun.edu/degrees?
+						{!! url('/degrees?') !!}
 						</div>
 					</div>
 
@@ -52,7 +52,7 @@
 					<strong>Use the generated URL as a query:</strong>
 					<div class="panel">
 						<div class="panel__content">
-						http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu
+						{!! url('/degrees?person=steven.fitzgerald@csun.edu') !!}
 						</div>
 					</div>
 
@@ -66,31 +66,53 @@
 						<ul class="list">
 							<li class="list__item">
 								<a href="{{ url('degrees?person=steven.fitzgerald@csun.edu') }}">
-									{{ url('degrees?person=steven.fitzgerald@csun.edu') }}</a>
+									{!! url('degrees?person=steven.fitzgerald@csun.edu') !!}</a>
 							</li>
 							<li class="list__item">
 								<a href="{{ url('degrees?person=son.pham@csun.edu') }}">
-									{{ url('degrees?person=son.pham@csun.edu') }}</a>
+									{!! url('degrees?person=son.pham@csun.edu') !!}</a>
 							</li>
 							<li class="list__item">
 								<a href="{{ url('degrees?person=rick.covington@csun.edu') }}">
-									{{ url('degrees?person=rick.covington@csun.edu') }}</a>
+									{!! url('degrees?person=rick.covington@csun.edu') !!}</a>
 							</li>
 						</ul>
 					<h2 id="code-examples" class="type--header type--thin">Code Examples</h2>
-					<div id="code-panels">
-						<ul class="nav nav-tabs">
-							<li role="presentation" class="active"><a aria-controls="php" role="tab" data-toggle="tab" href="#php">PHP</a></li>
-							<li role="presentation"><a aria-controls="javascript" role="tab" data-toggle="tab" href="#javascript">JavaScript</a></li>
-							<li role="presentation"><a aria-controls="ruby" role="tab" data-toggle="tab" href="#ruby">Ruby</a></li>
-							<li role="presentation"><a aria-controls="python" role="tab" data-toggle="tab" href="#python">Python</a></li>
-						</ul>
-						<div class="tab-content">
-							<div role="tabpanel" class="tab-pane fade in active" id="php">
-								<pre>
-									<code class="language-php">
+					<dl class="accordion">
+						<dt class="accordion__header"> JavaScript <i class="fa fa-chevron-down fa-pull-right type--red" aria-hidden="true"></i></dt>
+						<dd class="accordion__content">
+						<pre>
+					        <code class="prettyprint lang-js">
 //generate a url
-$url = 'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu';
+var url = '{!! url('degrees?person=steven.fitzgerald@csun.edu') !!}';
+
+//construct a function to get url and iterate over
+$(document).ready(function() {
+
+//use the URL as a request
+$.get(url, function(data) {
+
+  var degreeList = data.degrees;
+  //iterate over the degree list
+  $(degreeList).each(function(index, degree) {
+
+    //append each degree and institute
+    $('#degree-results').append(degree.degree + ' ' + degree.institute);
+
+  });
+
+});
+
+});
+							</code>
+						</pre>
+						</dd>
+						<dt class="accordion__header"> PHP <i class="fa fa-chevron-down fa-pull-right type--red" aria-hidden="true"></i></dt>
+						<dd class="accordion__content">
+							<pre>
+								<code class="prettyprint lang-php">
+//generate a url
+$url = '{!! url('degrees?person=steven.fitzgerald@csun.edu') !!}';
 
 //perform the query
 $data = file_get_contents($url);
@@ -101,48 +123,54 @@ $data = json_decode($data, true);
 //instantiate an empty array
 $degree_list = [];
 
-//iterate over the list of data 
+//iterate over the list of data
 foreach($data['degrees'] as $degree){
-		 $degree_list[] = $degree['degree'] . ' ' . $degree['institute'];
+	$degree_list[] = $degree['degree'] . ' ' . $degree['institute'];
 }
-									</code>
-								</pre>
-							</div>
-							<div role="tabpanel" class="tab-pane fade" id="javascript">
-								<pre>
-									<code class="language-javascript">
-//generate a url
-var url = 'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu';
+							</code>
+						</pre>
+						</dd>
+						<dt class="accordion__header"> Python <i class="fa fa-chevron-down fa-pull-right type--red" aria-hidden="true"></i></dt>
+  						<dd class="accordion__content">	
+							<pre>
+								<code class="prettyprint language-py">
+#python
+import urllib2
+import json
 
-//construct a function to get url and iterate over 
-$(document).ready(function() {
+#generate a url
+url = u'{!! url('degrees?person=steven.fitzgerald@csun.edu') !!}'
 
-	//use the URL as a request
-	$.get(url, function(data) {
+#open the url
+try:
+u = urllib2.urlopen(url)
+data = u.read()
+except Exception as e:
+data = {}
 
-		var degreeList = data.degrees;
-		//iterate over the degree list
-		$(degreeList).each(function(index, degree) {
-					
-			//append each degree and institute
-			$('#degree-results').append(degree.degree + ' ' + degree.institute);
+#load data with json object
+data = json.loads(data)
 
-		});
-		
-	});
+degrees_list = []
 
-});
-									</code>
-								</pre>
-							</div>
-							<div role="tabpanel" class="tab-pane fade" id="ruby">
-								<pre>
-									<code class="language-ruby">
+#iterate over the json object and add to the degrees list
+for degree in data['degrees']:
+degrees_list.append(degrees['degree'] + ' ' + degrees['institute'])
+
+print degrees_list
+
+								</code>
+							</pre>
+						</dd>
+  						<dt class="accordion__header"> Ruby <i class="fa fa-chevron-down fa-pull-right type--red" aria-hidden="true"></i></dt>
+  						<dd class="accordion__content">					   
+  							<pre>
+	  					        <code class="prettyprint lang-rb">
 require 'net/http'
 require 'json'
 
 #generate a url
-source = 'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu'
+source = '{!! url('degrees?person=steven.fitzgerald@csun.edu') !!}'
 
 #request the data
 response = Net::HTTP.get_response(URI.parse(source))
@@ -152,39 +180,8 @@ data = response.body
 
 #put the parsed data
 puts JSON.parse(data)
-									</code>
-								</pre>
-							</div>
-							<div role="tabpanel" class="tab-pane fade" id="python">
-								<pre>
-									<code class="language-python">
-#python
-import urllib2
-import json
-
-#generate a url
-url = u'http://degrees.ptg.csun.edu/degrees?person=steven.fitzgerald@csun.edu'
-
-#open the url 
-try:
-	 u = urllib2.urlopen(url)
-	 data = u.read()
-except Exception as e:
-	data = {}
-
-#load data with json object
-data = json.loads(data)
-
-degrees_list = []
-
-#iterate over the json object and add to the degrees list
-for degree in data['degrees']:
-		 degrees_list.append(degrees['degree'] + ' ' + degrees['institute'])
-
-print degrees_list
-								</code>
-			</pre>
-		</div>
-	</div>
-</div>
+							</code>
+						</pre>
+					</dd>
+					</dl>
 @stop
