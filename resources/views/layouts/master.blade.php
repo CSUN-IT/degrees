6708bf8740d04b380a59b28554c08542eace149b
luisjg <luis.guzman@metalab.csun.edu>
2017-07-31 13:26:47 -0700

    Update the tomorrow css.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,11 +9,7 @@
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic">
 	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="//cdn.metalab.csun.edu/metaphor/css/metaphor.css">
-	<link rel="stylesheet" type="text/css" href="//raw.githubusercontent.com/jmblog/color-themes-for-google-code-prettify/master/dist/themes/tomorrow.min.css">
-	<style type="text/css">
-		.metalab-footer .metalab-branding img {
-		  width: 110px; }
-	</style>
+	<link rel="stylesheet" type="text/css" href="{{ url('css/tomorrow.min.css') }}">
 </head>
 <body>
 	<div class="section section--sm">
