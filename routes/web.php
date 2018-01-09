b17bf20abc2e6546025ffdb26f5f74115f3d89ed
luisjg <luisjguzman87@gmail.com>
2018-01-09 14:26:57 -0800

    Re-order the routes to conform to lumen 5.5.

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+$router->get('/', function () {
+    $emails= [
+        'rick' => 'rick.covington@csun.edu',
+        'son' => 'son.pham@csun.edu',
+        'steve' => 'steven.fitzgerald@csun.edu'
+    ];
+    if(env('APP_ENV') !== 'production') {
+        foreach($emails as &$email)
+            $email = 'nr_'.$email;
+    }
+    return view('pages.landing-metaphor',compact('emails'));
+});
