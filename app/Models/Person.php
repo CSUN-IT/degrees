d3caad2a11614b779e5fc1fb6bf6d7fd43b9692a
luisjg <luisjguzman87@gmail.com>
2018-01-09 12:57:10 -0800

    Docblock and adjust the models.

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,14 +4,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model {
 
-	protected $table = 'fresco.people';			
-	protected $primaryKey = 'individuals_id';
-	protected $fillable = [];
+    /**
+     * Specify the table we want this model to reference
+     * as opposed to the default
+     *
+     * @var string
+     */
+    protected $table = 'fresco.people';
 
-	// only expose these attributes
+    /**
+     * Specify the primary key instead of the default
+     *
+     * @var string
+     */
+	protected $primaryKey = 'individuals_id';
+
+	/**
+     * Turn off the auto-incrementing feature of the
+     * primary key
+     *
+     * @var bool
+     */
+	public $incrementing = false;
+
+    /**
+     * The only attributes we want to see
+     *
+     * @var array
+     */
 	protected $visible = ['appt_year', 'degrees'];
 
-	public function degrees() {
+    /**
+     * The degree relationship to a person
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+	public function degrees()
+    {
 		return $this->hasMany('App\Models\Degree', 'individuals_id');
 	}
 }
