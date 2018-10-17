9e333679656704e69dd3447ffff9a63c8c082c17
luisjg <luis.guzman@metalab.csun.edu>
2018-10-17 15:33:52 -0700

    Update the docs to metaphor

--- resources/views/pages/about/version-history.blade.php
+++ resources/views/pages/about/version-history.blade.php
@@ -5,15 +5,13 @@
 @endsection
 
 @section('content')
-    <h2 id="introduction" class="type--header type--thin">Version History</h2>
-    <h2>{{ env('APP_NAME') }} 1.0.0 <small>Release Date: 02/06/18</small></h2>
-    <p>
-        <strong>Improvements:</strong>
-        <ol>
-            <li>Upgrade the underlying code base to the latest version.</li>
-            <li>HTTPS can now be enforced at the application level.</li>
-            <li>Include a version history.</li>
-        </ol>
-    </p>
-    <hr>
+    <h2 id="introduction">Version History</h2>
+    <h3 class="h5 padding">{{ env('APP_NAME') }} 1.0.0 <small>Release Date: 02/06/18</small></h3>
+    <strong>Improvements:</strong>
+    <ol>
+        <li>Upgrade the underlying code base to the latest version.</li>
+        <li>HTTPS can now be enforced at the application level.</li>
+        <li>Include a version history.</li>
+    </ol>
+    <hr class="margin">
 @endsection
